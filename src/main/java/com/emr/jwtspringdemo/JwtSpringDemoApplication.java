232c0e026cc2dad8b56c0022f481daea30163ec0
Emrullah <emr.ozk.71@gmail.com>
2022-07-24 18:01:33 +0300

    first commit

---
+++ src/main/java/com/emr/jwtspringdemo/JwtSpringDemoApplication.java
@@ -0,0 +1,13 @@
+package com.emr.jwtspringdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtSpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtSpringDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emr/jwtspringdemo/JwtSpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emr.jwtspringdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtSpringDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
